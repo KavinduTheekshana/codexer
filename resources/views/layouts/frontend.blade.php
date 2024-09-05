96e2f410d1ef2eb5027bf2b8872ada8b537ca434
Kavindu Theekshana <kavindutheekshana@gmail.com>
2024-09-05 13:02:56 +0100

    Meta

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -6,14 +6,33 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta content="" name="description" />
-    <meta content="" name="keywords" />
+    <meta name="keywords"
+        content="IT services, software development, web development, mobile app development, cloud solutions, IoT integration, data analytics, custom software, enterprise solutions, SaaS applications">
+    <meta name="description"
+        content="Codexer delivers top-tier IT services including custom software development, web and mobile app solutions, cloud integration, and advanced data analytics. Transform your business with our expertise.">
+
+
+    <meta property="og:title" content="Codexer | Transforming Ideas into Digital Reality">
+    <meta property="og:description"
+        content="Codexer delivers top-tier IT services including custom software development, web and mobile app solutions, cloud integration, and advanced data analytics.">
+    <meta property="og:image" content="{{ asset('favicon.svg') }}">
+    <meta property="og:url" content="https://www.codexer.co.uk">
+    <meta property="og:type" content="website">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Codexer | Transforming Ideas into Digital Reality">
+    <meta name="twitter:description" content="Codexer delivers top-tier IT services including custom software development, web and mobile app solutions, cloud integration, and advanced data analytics.">
+    <meta name="twitter:image" content="{{ asset('favicon.svg') }}">
+
+
     <!-- Favicons -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon.svg') }}">
 
     <!-- bootstrap grid css -->
     {{-- <link rel="stylesheet" href="{{ asset('frontend/css/plugins/bootstrap-grid.css') }}"> --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.min.css" integrity="sha512-i1b/nzkVo97VN5WbEtaPebBG8REvjWeqNclJ6AItj7msdVcaveKrlIIByDpvjk5nwHjXkIqGZscVxOrTb9tsMA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.min.css"
+        integrity="sha512-i1b/nzkVo97VN5WbEtaPebBG8REvjWeqNclJ6AItj7msdVcaveKrlIIByDpvjk5nwHjXkIqGZscVxOrTb9tsMA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-v4-grid-only@1.0.0/dist/bootstrap-grid.min.css"> --}}
     <!-- font awesome css -->
     <link rel="stylesheet" href="{{ asset('frontend/css/plugins/font-awesome.min.css') }}">
@@ -29,7 +48,7 @@
     @stack('styles')
     <title>Codexer | Transforming Ideas into Digital Reality</title>
 
-    @vite(['resources/js/app.js'])
+    {{-- @vite(['resources/js/app.js']) --}}
 </head>
 
 <body>
