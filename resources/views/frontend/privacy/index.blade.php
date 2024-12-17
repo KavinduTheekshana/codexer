7e107038f7ad631dd485a6116465ab65b7770f35
Kavindu Theekshana <kavindutheekshana@gmail.com>
2024-12-17 12:47:10 +0000

    Privacy Policy

--- resources/views/frontend/privacy/index.blade.php
+++ resources/views/frontend/privacy/index.blade.php
@@ -46,23 +46,27 @@
                     company.
                     You may, however, visit our site anonymously.</p>
 
-                <p class="mil-mb-30">We also collect standard information that your browser sends to every website you visit, such as your
+                <p class="mil-mb-30">We also collect standard information that your browser sends to every website you
+                    visit, such as your
                     IP address, browser type and language, access times, mouse interactions, and referring website
                     addresses. We do not link IP addresses to anything personally identifiable, meaning that user
                     sessions are tracked anonymously. Additionally, we track and analyze user actions on our website,
                     such as navigation, number of visits, downloads, and search items, to better understand visitor
                     behavior.</p>
 
-                <p class="mil-mb-30">The information we collect may be used to improve our website, send periodic emails, roll out
+                <p class="mil-mb-30">The information we collect may be used to improve our website, send periodic
+                    emails, roll out
                     contests, surveys, or other site features. You can opt out of receiving any of these communications
                     at any time.</p>
                 <h3 class="mil-up-font mil-mb-30">Storing Personal Information</h3>
-                <p class="mil-mb-30">Your personal information will be stored and kept for as long as it is required for the purpose it
+                <p class="mil-mb-30">Your personal information will be stored and kept for as long as it is required for
+                    the purpose it
                     was collected. This may result in information being retained until a request to unsubscribe is
                     received or you withdraw your consent to us holding your personal data.
                 </p>
                 <h3 class="mil-up-font mil-mb-30">Sharing Personal Information</h3>
-                <p class="mil-mb-30">Codexer is the sole owner of information collected on www.codexer.co.uk. Your information will not
+                <p class="mil-mb-30">Codexer is the sole owner of information collected on www.codexer.co.uk. Your
+                    information will not
                     be sold, traded, or transferred to third parties, except for our CRM solution provider where your
                     data is stored. Access to this information is restricted to authorized Codexer employees. However,
                     we may release your information when we believe it is appropriate to comply with the law, enforce
@@ -70,7 +74,8 @@
                     third-party service providers within the EU to drive lead generation initiatives, using our CRM to
                     collaborate on these activities.</p>
                 <h3 class="mil-up-font mil-mb-30">Security</h3>
-                <p class="mil-mb-30">We take reasonable measures to protect all personal information with confidentiality. We have
+                <p class="mil-mb-30">We take reasonable measures to protect all personal information with
+                    confidentiality. We have
                     implemented security measures to protect your information against loss, misuse, or alteration. All
                     actions taken to comply with GDPR are carried out in accordance with the processes of the ISO 27001
                     Information Security Management standard and ISO 27701 Privacy Information Management System.
@@ -79,18 +84,22 @@
                     Although we have implemented numerous security controls to protect your personal information, it is
                     impossible to guarantee 100% security.</p>
                 <h3 class="mil-up-font mil-mb-30">Rights to Personal Data</h3>
-                <p class="mil-mb-30">We acknowledge your right to access or rectify your personal data. You also have the right to
+                <p class="mil-mb-30">We acknowledge your right to access or rectify your personal data. You also have
+                    the right to
                     withdraw your consent to our processing of your personal data. You may object to the use of your
                     personal data by specifying aspects such as storage, transfer, or erasure. You can request access
                     to, rectification of, or deletion of your data from our databases at any time by emailing
                     info@codexer.co.uk. Your data will be subject to automatic deletion once the data retention period
                     for the initial purpose has expired.</p>
                 <h3 class="mil-up-font mil-mb-30">Changes to the Policy</h3>
-                <p class="mil-mb-30">We reserve the right to amend this privacy policy at any time. We will not send individual email
+                <p class="mil-mb-30">We reserve the right to amend this privacy policy at any time. We will not send
+                    individual email
                     notifications of updates. Any amendments will be posted on this page. You are therefore encouraged
                     to visit this page periodically.</p>
+
                 <h3 class="mil-up-font mil-mb-30">Contact Information</h3>
-                <p class="mil-mb-30">For more information about our privacy policy or to escalate any complaints, please contact our data
+                <p class="mil-mb-30">For more information about our privacy policy or to escalate any complaints, please
+                    contact our data
                     protection officer at info@codexer.co.uk. You also have the right to lodge a complaint with a
                     relevant supervisory authority if you identify that your personal information shared with us is
                     misused.</p>
